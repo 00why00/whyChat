19633a9f4a8fb1603538d06369c8242939646be7
why <894321963@163.com>
2019-06-11 11:55:49 +0800

    封装Activity和Fragment
    提升sdk版本到29，适配AndroidX

--- APP/whyChat/app/src/main/java/com/why/whychat/push/MainActivity.java
+++ APP/whyChat/app/src/main/java/com/why/whychat/push/MainActivity.java
@@ -1,13 +1,23 @@
 package com.why.whychat.push;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.why.whychat.common.app.Activity;
+
+import butterknife.BindView;
+
+public class MainActivity extends Activity {
+    @BindView(R.id.txt_test)
+    TextView mTsetText;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getContentLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initWidget() {
+        super.initWidget();
+        mTsetText.setText("Test Hello.");
     }
 }
---
+++ APP/whyChat/common/src/main/java/com/why/whychat/common/Common.java
@@ -0,0 +1,8 @@
+package com.why.whychat.common;
+
+/**
+ * @author why
+ */
+public class Common {
+
+}
